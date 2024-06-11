d8ae275a79f0d41c212c7401968a1d53410103da
Migero <mikeroche138@gmail.com>
2024-06-11 11:42:48 +0300

    update the path to the schema.sql file

--- lib/db/seed.py
+++ lib/db/seed.py
@@ -1,21 +1,14 @@
 # Importing necessary module for database management
 import sqlite3
 
-# Database file name
-DATABASE = 'pet_health_tracker.db'
+# Importing function to create database tables from models.py
+from models import create_tables  
 
-# Function to create database tables based on schema
-def create_tables():
-    # Connecting to the SQLite database
-    conn = sqlite3.connect(DATABASE)
-    
-    # Reading schema file and executing SQL commands
-    with open('lib/db/schema.sql') as f:
-        conn.executescript(f.read())
-    
-    # Closing the database connection
-    conn.close()
-
-# Entry point to execute script
-if __name__ == "__main__":
+# Main function to execute database seeding
+def main():
+    # Create tables based on the defined schema
     create_tables()
+
+# Entry point to execute the script
+if __name__ == "__main__":
+    main()
